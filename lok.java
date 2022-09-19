21623f331df50c92e27f813e9090ac95c065e768
lokesh <lokeshnrm07@gmail.com>
2022-09-19 10:23:18 +0530

    create java file and add

---
+++ lok.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class lok
+{
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        Double d = scan.nextDouble();
+        int i = scan.nextInt();
+ 
+
+        String s = scan.nextLine();
+        String s = scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+
+}
+
+
+/*
+import java.io.*;
+import java.util.*;
+
+public class lok {
+
+    public static void main(String[] args) {
+         //Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution.
+
+        Scanner sc=new Scanner(System.in);
+
+        int i=1;
+        if (sc.hasNext()) {
+            String s=sc.nextLine();
+            System.out.println(i + " " + s);
+            i++;
+        }
+    }
+}
+*/
+
+
