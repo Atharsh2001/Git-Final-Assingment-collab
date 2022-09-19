8adced69051d90f46e39e27c98956272372150c7
Priyadharshini M <m.priyadharshinimanak@gmail.com>
2022-09-19 15:05:49 +0530

    Smallest number program is added

---
+++ Small.java
@@ -0,0 +1,14 @@
+public class Small{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        if(a<b){
+            System.out.println("a is small");
+        }
+        else{
+            System.out.println("b is small");
+        }
+    }
+}
+//this program is basically is to find smallest number between two numbers
+//the inputs are given
