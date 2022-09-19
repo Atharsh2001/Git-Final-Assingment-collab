2c662abf971db3cafd5dd0f6c1b95361a5552f36
lokesh <lokeshnrm07@gmail.com>
2022-09-19 10:25:50 +0530

    add the java book file

---
+++ book.java
@@ -0,0 +1,20 @@
+package com.example.first_spring;
+import book.Bok;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class book {
+    @GetMapping("/book")
+    public List<Bok> getbook(){
+        List<Bok> list= Arrays.asList(
+                new Bok(1, "lokesh", "of 2 states", 2007,"fiction"),
+                new Bok(2, "llll", "1122", 2007,"fiction"),
+                new Bok(3, "oooo", "33234", 2007, "fiction")
+        );
+        return list;
+    }
+}
